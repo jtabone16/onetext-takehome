38998299e4f74d5206cd1e5788408d89f5c81ece
John Tabone <john@cashortrade.org>
2024-06-20 17:12:13 -0400

    make sure we're using react 18, use vite instead of CRA which was causing react 18 tailwind compatibility issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const container = document.getElementById('root');
+
+if (container) {
+    const root = createRoot(container);
+
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error('Failed to find the root element to mount the React app.');
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
